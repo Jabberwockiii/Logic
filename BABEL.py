4631661f917fd4777876efc2fb38b43fb2e842a4
Xu Zhikang <xuzhi@jacob>
2022-09-15 21:53:49 -0400

    logic2

---
+++ BABEL.py
@@ -0,0 +1,13 @@
+a = {
+    "set builder notation professor version"
+    : 
+    "the subset of A (such that) for all elements x in set A,"
+    "x is a memeber of the [set builder notation],"
+    "(if and only if), the subset of A that contains ALL and ONLY those objects x (for which) [condition A] x [condition B]"
+}
+b = {
+    "set builder notation my version":
+    "assumes that there is a set if condition A, then condition B. if condition B, then condtion A. Both A and B are assumptions about the subset relations."
+}
+def main():
+    print(a["set builder notation "])
